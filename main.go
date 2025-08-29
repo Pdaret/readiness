c66060dc7c1d8bdb59451d08b22f06b841e1c31d
Pdaret <mr.srezaei@outlook.com>
2025-08-29 13:11:44 +0330

    fix:

--- main.go
+++ main.go
@@ -2,64 +2,48 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"net"
 	"net/http"
+	"time"
 )
 
 func main() {
-	// // Step 1: Check marzban status
-	// for {
-	// 	cmd := exec.Command("sudo", "marzban", "status")
-	// 	output, err := cmd.Output()
-	// 	if err != nil {
-	// 		fmt.Println("Error executing marzban command:", err)
-	// 		time.Sleep(5 * time.Second) // Wait before retrying
-	// 		continue
-	// 	}
-
-	// 	if strings.Contains(string(output), "running") {
-	// 		break
-	// 	}
-
-	// 	fmt.Println("Marzban status not up, retrying...")
-	// 	time.Sleep(5 * time.Second) // Wait before retrying
-	// }
-
 	// Step 2: Extract ens5 inet IPv4 address
+	var ipv4Addr string
 	interfaces, err := net.Interfaces()
 	if err != nil {
-		fmt.Println("Error fetching network interfaces:", err)
+		fmt.Println("Error getting interfaces:", err)
 		return
 	}
 
-	var ipv4Addr string
 	for _, iface := range interfaces {
-		if iface.Name == "ens5" {
-			addrs, err := iface.Addrs()
-			if err != nil {
-				fmt.Println("Error getting addresses for ens5:", err)
-				return
+		// Skip down or loopback interfaces
+		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			fmt.Printf("Error getting addresses for interface %s: %v\n", iface.Name, err)
+			continue
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
 			}
 
-			for _, addr := range addrs {
-				var ip net.IP
-				switch v := addr.(type) {
-				case *net.IPNet:
-					ip = v.IP
-				case *net.IPAddr:
-					ip = v.IP
-				}
-
-				if ip != nil && ip.To4() != nil {
-					ipv4Addr = ip.String()
-					break
-				}
+			if ip != nil && ip.To4() != nil {
+				ipv4Addr = ip.String()
 			}
-			break
 		}
 	}
-
 	if ipv4Addr == "" {
 		fmt.Println("ens5 inet IPv4 address not found")
 		return
@@ -67,7 +51,9 @@ func main() {
 
 	fmt.Println("Extracted IPv4 Address:", ipv4Addr)
 
-	endpoint := fmt.Sprintf("https://phoenixstatus.com/api/v1/readiness/%s", ipv4Addr)
+	time.Sleep(5 * time.Minute)
+
+	endpoint := fmt.Sprintf("https://phoenixstatus.com/api/v1/ovh/readiness/%s", ipv4Addr)
 	// Step 3: Create HTTP request and send data to server
 	req, err := http.NewRequest("POST", endpoint, nil)
 	if err != nil {
@@ -75,6 +61,11 @@ func main() {
 		return
 	}
 	req.Header.Set("Content-Type", "application/json")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Minute)
+	defer cancel()
+
+	req = req.WithContext(ctx)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
